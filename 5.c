df1d2f2f202286a646a41a49dfed8e15819865cf
wengeapofferty <143488415+wengeapofferty@users.noreply.github.com>
2023-09-29 13:08:21 +0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
+
+int main() {
+    int num;
+    
+    printf("Введите число: ");
+    scanf("%d", &num);
+    
+    printf("Факториал %d: %d\n", num, factorial(num));
+    
+    return 0;
+}
