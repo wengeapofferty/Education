c20e17ca551fe9fca7af89c1035ccb0fefb7de89
wengeapofferty <143488415+wengeapofferty@users.noreply.github.com>
2024-02-10 14:17:03 +0300

    Add files via upload

---
+++ 43.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+int sumOfDigits(int num) {
+    int sum = 0;
+
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+int main() {
+    int num;
+    
+    printf("Введите число: ");
+    scanf("%d", &num);
+
+    int fact = factorial(num);
+    int result = sumOfDigits(fact);
+
+    printf("Сумма цифр факториала %d: %d\n", num, result);
+
+    return 0;
+}
