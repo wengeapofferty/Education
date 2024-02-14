d79cc1b8d511a81c513735f518981aa5984fc9f8
wengeapofferty <143488415+wengeapofferty@users.noreply.github.com>
2024-02-14 14:17:44 +0300

    Add files via upload

---
+++ 91.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Введите первое число: ");
+    scanf("%d", &num1);
+
+    printf("Введите второе число: ");
+    scanf("%d", &num2);
+
+    int result = gcd(num1, num2);
+    printf("НОД чисел %d и %d: %d\n", num1, num2, result);
+
+    return 0;
+}
