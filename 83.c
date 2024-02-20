ebe44a787a7f5e9cfadca362f9ba0ed204aaa46f
wengeapofferty <143488415+wengeapofferty@users.noreply.github.com>
2024-02-20 09:18:28 +0300

    Add files via upload

---
+++ 83.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int limit;
+    
+    printf("¬ведите число: ");
+    scanf("%d", &limit);
+
+    int sum = 0;
+
+    for (int i = 2; i < limit; ++i) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
+    printf("—умма простых чисел, меньших %d: %d\n", limit, sum);
+
+    return 0;
+}
